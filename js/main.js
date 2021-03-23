0a5ab224d42cb44b82ec4e5cf7b72e3ad1cc0cd4
Sazidul Alam <81089575+SazidulAlam47@users.noreply.github.com>
2021-03-23 23:37:47 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(window).load(function() {
+         $('#slider').nivoSlider();     
+});
+
+(function ($) {
+"use strict";
+
+/*--------------------------------------
+	mixitup Active
+----------------------------------------*/
+var mixer = mixitup('.portfolio-item',{
+	animation:{
+		effectsOut:'fade translateX(-100%)',
+	}
+});
+
+
+})(jQuery);
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+jQuery(document).ready(function($) {
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+  // Example: 
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href')); 
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 500);
+  }
+  $('[data-scroll]').on('click', scrollToSection);
+}(jQuery));
